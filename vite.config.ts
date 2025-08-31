710dee78acaf4b77838bf531a5e24fbc76923d26
ign24 <ignacio_zu@outlook.com>
2025-08-31 03:23:14 -0300

    fallback si el loadin screen no funciona

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   resolve: {
     alias: {
       '@': '/src',
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
     },
   },
 })
