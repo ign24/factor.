a6cb3d1d3d53d44a98ec139172bba948e5dfa459
ign24 <ignacio_zu@outlook.com>
2025-09-08 00:37:12 -0300

    solucionados errores de seguridad

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': resolve(__dirname, 'src'),
+      'vue': 'vue/dist/vue.esm-bundler.js',
     },
   },
   build: {
